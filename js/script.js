9b7bab8235c1843d69dcd84e69b342c71c5c6aee
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-18 22:22:26 -0800

    add JS functionality

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.js-nav');
+const hamburger = document.querySelector('.js-hamburger');
+const hamburgerBtn = document.querySelector('.js-hamburger-btn');
+
+hamburgerBtn.addEventListener('click', () => {
+  if (!nav.classList.contains('nav--is-open')) {
+    nav.classList.add('nav--is-open');
+    hamburger.src = '../images/icon-close.svg';
+    hamburgerBtn.setAttribute('aria-expanded', true);
+  } else {
+    nav.classList.remove('nav--is-open');
+    hamburger.src = '../images/icon-hamburger.svg';
+    hamburgerBtn.setAttribute('aria-expanded', false);
+  }
+});
